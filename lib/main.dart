7e551828a573e86d2a48765cb354448cef84b06b
Lester Dela Cruz <bboyjacks@yahoo.com>
2019-05-24 15:16:13 -0700

    Error on redirecting sign in view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'barchart.dart';
+import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -17,16 +20,56 @@ class MyApp extends StatelessWidget {
           title: Text("Bar Chart"),
         ),
         body: SafeArea(
-          child: Center(
-            child: DebugContainer(
-              child: BarChart(
-                data: [50, 40, 70, 60, 60, 45]
-              )
-            )
-          )
+          child: StartPage()
         ),
       ),
     );
+  }
+}
+
+class StartPage extends StatefulWidget {
+  final ClientId clientId = ClientId("357926066084-rhm1hm673ihal0ere5v9u35t6pmr2e1r.apps.googleusercontent.com", "baEPaU-19Kev0nNM4B4JVCjr");
+  final List<String> scopes = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  http.Client client = new http.Client();
+  Widget startWidget = Center(child: Text("Test"),);
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  @override
+  void initState() {
+    startClientAuth();
+    super.initState();
+  }
+
+  void startClientAuth() {
+    obtainAccessCredentialsViaUserConsent(widget.clientId, widget.scopes, client, openWebview)
+        .then((AccessCredentials credentials) {
+      print("Authenticated");
+      client.close();
+    });
+  }
+
+  void openWebview(String url) {
+    print(url);
+    setState(() {
+     startWidget = WebView(
+      initialUrl: url,
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller.complete(webViewController);
+      },
+     );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return startWidget;
   }
 }
 
